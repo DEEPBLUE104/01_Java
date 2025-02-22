6246ba0a9d8b5c1e5c6505a4d30962c3c6ed5d09
DEEPBLUE104 <dlara7173@gmail.com>
2025-02-22 16:43:15 +0900

    oop 실습문제

---
+++ 06_OOP/src/edu/kh/oop/practice/model/service/BookService.java
@@ -0,0 +1,13 @@
+package edu.kh.oop.practice.model.service;
+
+import edu.kh.oop.practice.model.vo.Book;
+
+public class BookService {
+
+	private Book bk = new Book();
+	
+	public BookService() {	}
+		
+		
+	}
+

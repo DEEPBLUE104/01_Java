ef03e4d970dab093e48ca56c45ffaa295c8edda7
DEEPBLUE104 <dlara7173@gmail.com>
2025-02-28 10:41:46 +0900

    학생관리시스템

--- 11_collection/src/edu/kh/collection/pack1/run/StudentRun.java
+++ 11_collection/src/edu/kh/collection/pack1/run/StudentRun.java
@@ -1,5 +1,10 @@
 package edu.kh.collection.pack1.run;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 import edu.kh.collection.pack1.model.service.StudentService;
 
 public class StudentRun {
@@ -7,7 +12,21 @@ public class StudentRun {
 	
 		StudentService service = new StudentService();
 		// service.ex();
-		service.displayMenu();
+		
+		List<Integer> numberList = new ArrayList<Integer>();
+		
+		numberList.add(7); //0
+		numberList.add(3); //1
+		numberList.add(1); //2
+		numberList.add(6); //3
+		numberList.add(8); //4
+		
+		// 숫자 리스트를 정렬 
+		//Integer는 이미 Comparable을 상속받아 compareTo() 정의
+		//Collections.sort(numberList);
+	
+		//		System.out.println(numberList);
+	
 	}
 	
 }
